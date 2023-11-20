20e4924633f530c924a3cd59a8291393272345f9
MetaReal <mhershadi84@gmail.com>
2023-11-20 11:14:11 +0330

    Fixed sse, avx2, and avx512 macro checks

--- heads/mrfstr-intern.h
+++ heads/mrfstr-intern.h
@@ -9,7 +9,7 @@
 #include <mrfstr.h>
 #include <immintrin.h>
 
-#ifdef __AVX512F__
+#ifdef __AVX512BW__
 
 typedef __m512i mrfstr_simd_block_t;
 
@@ -41,8 +41,8 @@ typedef __m256i mrfstr_simd_block_t;
 
 #define mrfstr_simd_set1_func _mm256_set1_epi8
 #define mrfstr_simd_stream_func _mm256_stream_si256
-#define mrfstr_simd_cmpneq_func _mm256_cmpneq_epi64_mask
-#define mrfstr_simd_cmpeq_mask_func _mm256_cmpeq_epi8_mask
+#define mrfstr_simd_cmpneq_func(x, y) (~_mm256_movemask_epi8(_mm256_cmpeq_epi8(x, y)))
+#define mrfstr_simd_cmpeq_mask_func(x, y) _mm256_movemask_epi8(_mm256_cmpeq_epi8(x, y))
 
 #else
 
@@ -58,8 +58,8 @@ typedef __m128i mrfstr_simd_block_t;
 
 #define mrfstr_simd_set1_func _mm_set1_epi8
 #define mrfstr_simd_stream_func _mm_stream_si128
-#define mrfstr_simd_cmpneq_func _mm_cmpneq_epi64_mask
-#define mrfstr_simd_cmpeq_mask_func _mm_cmpeq_epi8_mask
+#define mrfstr_simd_cmpneq_func(x, y) (~_mm_movemask_epi8(_mm_cmpeq_epi8(x, y)))
+#define mrfstr_simd_cmpeq_mask_func(x, y) _mm_movemask_epi8(_mm_cmpeq_epi8(x, y))
 
 #endif
 #endif
--- heads/mrtstr-intern.h
+++ heads/mrtstr-intern.h
@@ -9,7 +9,7 @@
 #include <mrtstr.h>
 #include <immintrin.h>
 
-#ifdef __AVX512F__
+#ifdef __AVX512BW__
 
 typedef __m512i mrtstr_simd_block_t;
 
@@ -37,8 +37,8 @@ typedef __m256i mrtstr_simd_block_t;
 
 #define mrtstr_simd_set1_func _mm256_set1_epi8
 #define mrtstr_simd_stream_func _mm256_stream_si256
-#define mrtstr_simd_cmpneq_func _mm256_cmpneq_epi64_mask
-#define mrtstr_simd_cmpeq_mask_func _mm256_cmpeq_epi8_mask
+#define mrtstr_simd_cmpneq_func(x, y) (~_mm256_movemask_epi8(_mm256_cmpeq_epi8(x, y)))
+#define mrtstr_simd_cmpeq_mask_func(x, y) _mm256_movemask_epi8(_mm256_cmpeq_epi8(x, y))
 
 #else
 
@@ -52,8 +52,8 @@ typedef __m128i mrtstr_simd_block_t;
 
 #define mrtstr_simd_set1_func _mm_set1_epi8
 #define mrtstr_simd_stream_func _mm_stream_si128
-#define mrtstr_simd_cmpneq_func _mm_cmpneq_epi64_mask
-#define mrtstr_simd_cmpeq_mask_func _mm_cmpeq_epi8_mask
+#define mrtstr_simd_cmpneq_func(x, y) (~_mm_movemask_epi8(_mm_cmpeq_epi8(x, y)))
+#define mrtstr_simd_cmpeq_mask_func(x, y) _mm_movemask_epi8(_mm_cmpeq_epi8(x, y))
 
 #endif
 #endif

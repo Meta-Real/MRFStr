816cfe6c2c414452cb07772c74be624f7cfc2d26
Meta Real <mhershadi84@gmail.com>
2024-02-07 23:35:20 +0330

    Changed msvcsetup to use cpuid functions

--- msvcsetup.c
+++ msvcsetup.c
@@ -15,94 +15,62 @@ copies or substantial portions of the Software.
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <cpuid/cpuid.h>
 
 #if defined(__GNUC__) || defined(__clang__)
 #define MRFSTR_GCC_CLANG
-#endif
-
-#ifdef MRFSTR_GCC_CLANG
-#include <cpuid.h>
-#elif defined(_MSC_VER)
-#include <intrin.h>
-#else
-#error Your compiler is not supported yet
 #endif
 
 int main(void)
 {
     FILE *header = fopen("heads/simddef.h", "w");
     if (!header)
-        return EXIT_FAILURE;
+        return 1;
 
     fputs("#ifndef __MRFSTR_SIMDDEF__\n"
         "#define __MRFSTR_SIMDDEF__\n\n", header);
 
-    int data[4];
-
-#ifdef MRFSTR_GCC_CLANG
-    __cpuid(0, data[0], data[1], data[2], data[3]);
-#elif defined(_MSC_VER)
-    __cpuid(data, 0);
-#endif
-
-    int nid = data[0];
-    if (nid >= 1)
+    mrfstr_byte_t simd = mrfstr_get_simdset();
+    if (simd >= 1)
+        fputs("#ifndef __SSE2__\n"
+            "#define __SSE2__\n"
+            "#endif\n\n", header);
+    if (simd >= 2)
+        fputs("#ifndef __SSSE3__\n"
+            "#define __SSSE3__\n"
+            "#endif\n\n", header);
+    if (simd >= 3)
+        fputs("#ifndef __SSE4_1__\n"
+            "#define __SSE4_1__\n"
+            "#endif\n\n", header);
+    if (simd >= 4)
+        fputs("#ifndef __AVX__\n"
+            "#define __AVX__\n"
+            "#endif\n\n", header);
+    if (simd >= 5)
+        fputs("#ifndef __AVX2__\n"
+            "#define __AVX2__\n"
+            "#endif\n\n", header);
+    if (simd >= 6)
+        fputs("#ifndef __AVX512F__\n"
+            "#define __AVX512F__\n"
+            "#endif\n\n", header);
+    if (simd >= 7)
     {
-        __cpuidex(data, 1, 0);
-
-        int reg = data[2];
-        if (reg & 0x200)
-            fputs("#ifndef __SSSE3__\n"
-                "#define __SSSE3__\n"
-                "#endif\n\n", header);
-        if (reg & 0x80000)
-            fputs("#ifndef __SSE4_1__\n"
-                "#define __SSE4_1__\n"
-                "#endif\n\n", header);
-        if (reg & 0x10000000)
-            fputs("#ifndef __AVX__\n"
-                "#define __AVX__\n"
-                "#endif\n\n", header);
-
-        reg = data[3];
-        if (reg & 0x4000000)
-            fputs("#ifndef __SSE2__\n"
-                "#define __SSE2__\n"
-                "#endif\n\n", header);
-
-        if (nid >= 7)
-        {
-            __cpuidex(data, 7, 0);
-
-            reg = data[1];
-            if (reg & 0x20)
-                fputs("#ifndef __AVX2__\n"
-                    "#define __AVX2__\n"
-                    "#endif\n\n", header);
-            if (reg & 0x10000)
-                fputs("#ifndef __AVX512F__\n"
-                    "#define __AVX512F__\n"
-                    "#endif\n\n", header);
-            if (reg & 0x40000000)
-                fputs("#ifndef __AVX512BW__\n"
-                    "#define __AVX512BW__\n"
-                    "#endif\n\n", header);
-            if (reg & 0x80000000)
-                fputs("#ifndef __AVX512VL__\n"
-                    "#define __AVX512VL__\n"
-                    "#endif\n\n", header);
-
-            reg = data[2];
-            if (reg & 2)
-                fputs("#ifndef __AVX512VBMI__\n"
-                    "#define __AVX512VBMI__\n"
-                    "#endif\n\n", header);
-        }
+        fputs("#ifndef __AVX512BW__\n"
+            "#define __AVX512BW__\n"
+            "#endif\n\n", header);
+        fputs("#ifndef __AVX512VL__\n"
+            "#define __AVX512VL__\n"
+            "#endif\n\n", header);
     }
+    if (simd >= 8)
+        fputs("#ifndef __AVX512VBMI__\n"
+            "#define __AVX512VBMI__\n"
+            "#endif\n\n", header);
 
     fputs("#endif\n", header);
 
     fclose(header);
-    return EXIT_SUCCESS;
+    return 0;
 }
